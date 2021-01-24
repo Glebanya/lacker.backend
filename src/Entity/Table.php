5b18913fd9e96bb1a2d9bd8c9167e8cb03fc1daf
uzver <azzar.bam@gmail.com>
2021-01-24 20:28:59 +0200

    entity: added table orm entity

---
+++ src/Entity/Table.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TableRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidV4Generator;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * @ORM\Entity(repositoryClass=TableRepository::class)
+ * @ORM\Table(name="`lacker_table`")
+ */
+class Table
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UuidV4Generator::class)
+     */
+    private ?Uuid $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $max_persons;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private ?string $status;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $number;
+
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function getMaxPersons(): ?int
+    {
+        return $this->max_persons;
+    }
+
+    public function setMaxPersons(int $max_persons): self
+    {
+        $this->max_persons = $max_persons;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+    
+}
