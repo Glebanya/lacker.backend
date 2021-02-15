b33008bfc58b447320cbf53fab449709f089f279
uzver <azzar.bam@gmail.com>
2021-02-16 01:31:21 +0200

    general: added exception handler

---
+++ src/Api/Event/ExceptionHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Api\Event;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionHandler
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        $message = sprintf(
+            'My Error says: %s with code: %s',
+            $exception->getMessage(),
+            $exception->getCode()
+        );
+
+        $response = new Response();
+        $response->setContent($message);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // sends the modified response object to the event
+        $event->setResponse($response);
+    }
+}
