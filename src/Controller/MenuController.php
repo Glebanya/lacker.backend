6f55df9d3a69ff8d1de92808aec0dae95a45b252
uzver <azzar.bam@gmail.com>
2021-02-16 01:32:00 +0200

    controllers: added routes to menu endpoints

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -3,28 +3,50 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MenuController extends AbstractController
 {
     /**
-     * @Route("/menu/{id}", name="menu")
+     * @Route("/menu/{id}", name="menu_get", methods={"GET"})
      * @param string $id
+     * @param Request $request
      * @return Response
      */
-    public function index(string $id): Response
-    {
-        return $this->json([
-            //'message' => $id,
-        ]);
-    }
-    /*
-    /**
-     * @Route ()
-     */
-    /*public function menu($id)
+    public function getMenu(string $id,Request $request): Response
     {
 
-    }*/
+    }
+
+    /**
+     * @Route("/menu", name="menu_add", methods={"GET"})
+     * @param Request $request
+     * @return Response
+     */
+    public function addMenu(Request $request): Response
+    {
+
+    }
+
+    /**
+     * @Route("/menu/{id}", name="menu_delete", methods={"DELETE"})
+     * @param Request $request
+     * @return Response
+     */
+    public function deleteMenu(Request $request): Response
+    {
+
+    }
+
+    /**
+     * @Route("/menu/{id}", name="menu_update", methods={"GET"})
+     * @param Request $request
+     * @return Response
+     */
+    public function updateMenu(Request $request): Response
+    {
+
+    }
 }
