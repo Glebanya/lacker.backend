31288c495989ab8bcdacb800350f58f7cb0b8f85
uzver <azzar.bam@gmail.com>
2021-03-06 20:35:48 +0200

    controllers: changed itokenable inteface

--- src/Api/Event/TokenEventSubscriber.php
+++ src/Api/Event/TokenEventSubscriber.php
@@ -11,39 +11,60 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
+use App\Api\Auth\Access\AccessValidator;
 
 class TokenEventSubscriber implements EventSubscriberInterface
 {
+
+    private AccessValidator $validator;
+
     private EntityManagerInterface $manager;
 
     private $user;
 
-    public function __construct(EntityManagerInterface $entityManager){
-        $this->manager = $entityManager;
+    private  Auth\AuthHandlerObject $authObject;
+
+    private function validateAccessToken(string $token) : bool {
+        return $this->authObject->setKey($token)->validate();
     }
 
-    public function onKernelController(ControllerEvent $event)
-    {
+    private function findUser(): ?object {
+        $params = $this->authObject->getParams();
+        return is_array($params) && array_key_exists('user_id', $params)?
+            $this->manager->getRepository(Client::class)->find($params['user_id']) :
+            null;
+    }
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        AccessValidator $validator,
+        Auth\AuthHandlerObject $authObject
+    ){
+        $this->manager = $entityManager;
+        $this->validator = $validator;
+        $this->authObject = $authObject;
+    }
+
+    public function onKernelController(ControllerEvent $event) {
+
         if (is_array($controller = $event->getController())) {
-            $controller = $controller[0];
+            [$controller,$method] = $controller;
         }
 
-        if ($controller instanceof ITokenController){
-
-            $token = $event->getRequest()->query->get('access_token');
-            $authObject = Auth\AuthHandlerObject::create();
-            if (!$token || !$authObject->setKey($token)->validate() && array_key_exists('user_id',$authObject->getParams() ?? [])) {
-
+        if ($controller instanceof ITokenController && in_array($method, $controller->getNonPublicMethods())) {
+            if (
+                !$event->getRequest()->request->has('access_token') &&
+                !$this->validateAccessToken($event->getRequest()->request->get('access_token'))
+            ){
                 throw new AccessDeniedHttpException('This action needs a valid token!');
-
-            } elseif (!($this->user = $this->manager->getRepository(Client::class)->find($authObject->getParams()['user_id']))) {
+            } elseif (!$this->user = $this->findUser()) {
                 throw new AccessDeniedHttpException('This user not exists!');
             }
             $controller->setAccessController($this);
         }
     }
 
-    public function getUser(){
+    public function getUser() : ?Client {
         return $this->user;
     }
 
