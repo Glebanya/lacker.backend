a6d3aea3712bb8acdf1525b2b0c475dce26d86d3
uzver <azzar.bam@gmail.com>
2021-03-07 01:03:31 +0200

    changed migration

--- migrations/Version20210306230217.php
+++ migrations/Version20210306230217.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210306224356 extends AbstractMigration
+final class Version20210306230217 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20210306224356 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE access (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', user_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', business_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', role VARCHAR(255) NOT NULL, INDEX IDX_6692B54A76ED395 (user_id), INDEX IDX_6692B54A89DB457 (business_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE access (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', user_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', business_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', role INT NOT NULL, INDEX IDX_6692B54A76ED395 (user_id), INDEX IDX_6692B54A89DB457 (business_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE access ADD CONSTRAINT FK_6692B54A76ED395 FOREIGN KEY (user_id) REFERENCES `lacker_client` (id)');
         $this->addSql('ALTER TABLE access ADD CONSTRAINT FK_6692B54A89DB457 FOREIGN KEY (business_id) REFERENCES business (id)');
     }
