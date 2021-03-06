3568e6f01d943f3f5dfad2d5e08b0514337dc22b
uzver <azzar.bam@gmail.com>
2021-03-07 00:45:13 +0200

    added access table

---
+++ src/Entity/Access.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccessRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidV4Generator;
+
+/**
+ * @ORM\Entity(repositoryClass=AccessRepository::class)
+ */
+class Access
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UuidV4Generator::class)
+     */
+    private ?Uuid $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Client::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private ?Client $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Business::class, inversedBy="accesses")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private ?Business $business;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private ?string $role;
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?Client
+    {
+        return $this->user;
+    }
+
+    public function setUser(?Client $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getBusiness(): ?Business
+    {
+        return $this->business;
+    }
+
+    public function setBusiness(?Business $business): self
+    {
+        $this->business = $business;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
