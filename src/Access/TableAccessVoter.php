bc5a931d713326350ebbf6201de518e73f08bd3f
glebanya <azzar.bam@gmail.com>
2021-05-15 15:41:43 +0200

    ._.

---
+++ src/Access/TableAccessVoter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Access;
+
+use App\Entity\Staff;
+use App\Entity\Table;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class TableAccessVoter extends Voter
+{
+	protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+	{
+		return match ($attribute) {
+			'view' => $this->canView($subject,$token),
+			'update' => $this->canUpdate($subject,$token),
+			'delete' => $this->canDelete($subject,$token),
+			default => false
+		};
+	}
+
+	protected function canView($subject, TokenInterface $token): bool
+	{
+		return true;
+	}
+
+	protected function canUpdate($subject, TokenInterface $token): bool
+	{
+		return
+			$subject instanceof Staff and ($user = $token->getUser()) and $user instanceof Staff and
+			$user->getRestaurant()->getId()->compare($subject->getRestaurant()->getId()) === 0 and
+			(in_array($user->getRole(),[Staff::ROLE_ADMINISTRATOR, Staff::ROLE_MANAGER]) or $user->isEqualTo($subject))
+			;
+	}
+
+	protected function canDelete($subject, TokenInterface $token): bool
+	{
+		return
+			$subject instanceof Staff and ($user = $token->getUser()) and $user instanceof Staff and
+			$user->getRestaurant()->getId()->compare($subject->getRestaurant()->getId()) === 0 and
+			in_array($user->getRole(),[Staff::ROLE_ADMINISTRATOR])
+			;
+	}
+
+	protected function supports(string $attribute, $subject): bool
+	{
+		return $subject instanceof Table;
+	}
+}
